4fd3f3f6af209fd7c6ce12f959934c6f2152f0dc
VS <vs@vss-macbook-air.mynet>
2023-04-23 19:24:04 +0530

    Updates

---
+++ precache-manifest.ffbcdb43c65bc08d2c2fa3cdae46409d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0454f9fff4bae2669f73cc16234f869",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8cbb28342dab0a843af",
+    "url": "/static/css/main.8af960f8.chunk.css"
+  },
+  {
+    "revision": "30aba62aa366abe95745",
+    "url": "/static/js/2.14100a6c.chunk.js"
+  },
+  {
+    "revision": "e8cbb28342dab0a843af",
+    "url": "/static/js/main.043fda04.chunk.js"
+  },
+  {
+    "revision": "8dcc8ff983cc2f73280b",
+    "url": "/static/js/runtime~main.3d643612.js"
+  },
+  {
+    "revision": "9f92eba12e9aaeb21c303341ceaed8ad",
+    "url": "/static/media/bg4-dark.9f92eba1.jpg"
+  },
+  {
+    "revision": "f6ebd87087406b5d868eeaa2b6787467",
+    "url": "/static/media/bg4.f6ebd870.jpg"
+  },
+  {
+    "revision": "fd3af4a0cf5cc67bd2bbb6722d8dcfb8",
+    "url": "/static/media/favicon.fd3af4a0.png"
+  },
+  {
+    "revision": "cf1fbe504cabb3271af9d28cfbe8994b",
+    "url": "/static/media/team_barbara.cf1fbe50.jpg"
+  },
+  {
+    "revision": "838271f2bb5fd480f8024ed0d9346610",
+    "url": "/static/media/team_kindra.838271f2.jpg"
+  },
+  {
+    "revision": "e2251d4e8c760d0369aee149ceabcfb1",
+    "url": "/static/media/team_ritu.e2251d4e.jpg"
+  },
+  {
+    "revision": "b735faaa2ddbe2b25bec96bcf0660aac",
+    "url": "/static/media/team_veena.b735faaa.jpg"
+  },
+  {
+    "revision": "a857b1ab4b8e61026c42b9a955d482c4",
+    "url": "/static/media/whitebg.a857b1ab.jpg"
+  }
+]);
